5a9057fbde321abbb9ce927b4a535e8f4807e3c1
ammad <ammad@10.0.0.2>
2017-08-15 22:26:54 -0500

    Added MyController Class to the bean and used it,

---
+++ SpringDependency/src/main/java/com/main/SpringDependency/Controller/MyController.java
@@ -0,0 +1,14 @@
+package com.main.SpringDependency.Controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MyController {
+	
+	public void hello() {
+		
+		System.out.println("Hello World");
+		
+	}
+
+}
--- SpringDependency/src/main/java/com/main/SpringDependency/SpringDependencyApplication.java
+++ SpringDependency/src/main/java/com/main/SpringDependency/SpringDependencyApplication.java
@@ -2,11 +2,19 @@ package com.main.SpringDependency;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import com.main.SpringDependency.Controller.MyController;
+
+
 
 @SpringBootApplication
 public class SpringDependencyApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringDependencyApplication.class, args);
+		ApplicationContext ctx = SpringApplication.run(SpringDependencyApplication.class, args);
+		
+	MyController con = (MyController)ctx.getBean("myController");
+		con.hello();
 	}
 }
