23bf7c11e6e4acf987aeb51e2f731ac3c56e63d1
ammad <ammad@10.0.0.2>
2017-09-10 14:01:35 -0500

    Demonstration of Singleton and Prototype Scope

--- SpringScopeBeanDemo/src/main/java/com/BeanScope/SpringScopeBeanDemo/Beans/CounterObj.java
+++ SpringScopeBeanDemo/src/main/java/com/BeanScope/SpringScopeBeanDemo/Beans/CounterObj.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
-//@Scope("prototype")
+//@Scope("prototype") Scope
 public class CounterObj {
 
 public int count;
