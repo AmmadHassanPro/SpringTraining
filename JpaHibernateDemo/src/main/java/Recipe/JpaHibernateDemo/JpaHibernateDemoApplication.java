24b1a0209c62ac3d6db5a9ae70cb10776f235b3e
ammad <ammad@10.0.0.22>
2017-09-18 21:46:50 -0500

    Added Ingredients, Notes, Recipes Entities and managed the relationships between them

---
+++ JpaHibernateDemo/src/main/java/Recipe/JpaHibernateDemo/JpaHibernateDemoApplication.java
@@ -0,0 +1,12 @@
+package Recipe.JpaHibernateDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaHibernateDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaHibernateDemoApplication.class, args);
+	}
+}
