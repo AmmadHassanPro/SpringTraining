ca50c0217218b9ea6f8c308229013c8646184924
ammad <ammad@10.0.0.22>
2017-09-19 22:09:15 -0500

    H2 console is working

---
+++ JpaHibernateDemo/src/main/java/Recipe/JpaHibernateDemo/Controller/IndexController.java
@@ -0,0 +1,20 @@
+package Recipe.JpaHibernateDemo.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+
+public class IndexController {
+
+
+
+    @RequestMapping({"", "/", "/index"})
+
+    public String getIndexPage(){
+
+        return "index";
+
+    }
+
+}
