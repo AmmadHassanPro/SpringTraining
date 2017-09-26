6b8b03b8b587032579476f1d5bbb1eb24c9d23a8
ammad <ammad@10.0.0.22>
2017-09-25 22:01:57 -0500

    Added Exercise save (Exercise excersice); method to Excersice Service Interface.

--- spring_mvc_base-master/src/main/java/com/MVCHibernate/service/ExerciseService.java
+++ spring_mvc_base-master/src/main/java/com/MVCHibernate/service/ExerciseService.java
@@ -3,9 +3,11 @@ package com.MVCHibernate.service;
 import java.util.List;
 
 import com.MVCHibernate.model.Activity;
+import com.MVCHibernate.model.Exercise;
 
 public interface ExerciseService {
 
 	List<Activity> findAllActivities();
+	Exercise save (Exercise excersice);
 
 }
