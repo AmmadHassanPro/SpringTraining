13314318361a06287ef3b1d311e8968ebe181913
ammad <ammad@10.0.0.22>
2017-09-25 22:03:13 -0500

    Added 	private List<Exercise> exercises; field and established One to Many relationship with Goal.

--- spring_mvc_base-master/src/main/java/com/MVCHibernate/model/Goal.java
+++ spring_mvc_base-master/src/main/java/com/MVCHibernate/model/Goal.java
@@ -1,8 +1,11 @@
 package com.MVCHibernate.model;
 
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import org.hibernate.validator.constraints.Range;
@@ -14,11 +17,30 @@ public class Goal {
 	@Id
 	@GeneratedValue
 	private Long id;
+	@OneToMany(mappedBy="goal")
+	private List<Exercise> exercises;
+	
 	@Range(min = 1, max = 120)
 	private int minutes;
 
 	public int getMinutes() {
 		return minutes;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public List<Exercise> getExercises() {
+		return exercises;
+	}
+
+	public void setExercises(List<Exercise> exercises) {
+		this.exercises = exercises;
 	}
 
 	public void setMinutes(int minutes) {
