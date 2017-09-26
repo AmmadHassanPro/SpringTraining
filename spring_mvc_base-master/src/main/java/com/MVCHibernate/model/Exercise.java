c83c1dc2edc12b36d6708ad4e0d16974d9248a87
ammad <ammad@10.0.0.22>
2017-09-25 22:01:21 -0500

    Added ID field and goal field with associated annotation to establish the relation between excercise and goal (Many to one)

--- spring_mvc_base-master/src/main/java/com/MVCHibernate/model/Exercise.java
+++ spring_mvc_base-master/src/main/java/com/MVCHibernate/model/Exercise.java
@@ -1,15 +1,38 @@
 package com.MVCHibernate.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.validation.constraints.NotNull;
 
 import org.hibernate.validator.constraints.Range;
 
-
+@Entity
 public class Exercise {
-	
+	@Id
+	@GeneratedValue
+	private Long id;
 	@Range(min = 1, max = 120)
 	private int minutes;
-	
+	@ManyToOne
+	private Goal goal;
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Goal getGoal() {
+		return goal;
+	}
+
+	public void setGoal(Goal goal) {
+		this.goal = goal;
+	}
+
 	@NotNull
 	private String activity;
 
