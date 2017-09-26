ad7de6c965d22077452d73ec5439c436a3e89f2d
ammad <ammad@10.0.0.22>
2017-09-25 20:28:14 -0500

    Fixed anootaion on EnitityManager

--- spring_mvc_base-master/src/main/java/com/MVCHibernate/repository/GoalRepositoryImpl.java
+++ spring_mvc_base-master/src/main/java/com/MVCHibernate/repository/GoalRepositoryImpl.java
@@ -1,21 +1,24 @@
 package com.MVCHibernate.repository;
 
 import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
 
 import com.MVCHibernate.model.Goal;
-
+@Repository
 public class GoalRepositoryImpl implements GoalRepository {
 
-	@Autowired
+	@PersistenceContext
 	private EntityManager em;
 	
 	public Goal save(Goal goal) {
 
 
 		em.persist(goal);
-		
+		em.flush();
 		
 		return null;
 	}
