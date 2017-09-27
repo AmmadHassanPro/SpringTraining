71dc2ce0477e5102881ec1f2f52e5b9f925c7555
ammad <ammad@10.0.0.22>
2017-09-26 22:31:50 -0500

    added getAllGoals method on Goal Repository Interface.

--- spring_mvc_base-master/src/main/java/com/MVCHibernate/repository/GoalRepository.java
+++ spring_mvc_base-master/src/main/java/com/MVCHibernate/repository/GoalRepository.java
@@ -1,9 +1,12 @@
 package com.MVCHibernate.repository;
 
+import java.util.List;
+
 import org.springframework.stereotype.Repository;
 
 import com.MVCHibernate.model.Goal;
 
 public interface GoalRepository {
 	Goal save(Goal goal);
+	 List getAllGoalls(); 
 }
