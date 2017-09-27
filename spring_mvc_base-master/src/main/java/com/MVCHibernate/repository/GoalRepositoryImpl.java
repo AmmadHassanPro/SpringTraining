3b623c79ead649a09a65a4ed3e838a8fcf4b7c62
ammad <ammad@10.0.0.22>
2017-09-26 22:32:34 -0500

    Implemented getAllGoals on Goal RepositoryImpl class.

--- spring_mvc_base-master/src/main/java/com/MVCHibernate/repository/GoalRepositoryImpl.java
+++ spring_mvc_base-master/src/main/java/com/MVCHibernate/repository/GoalRepositoryImpl.java
@@ -2,12 +2,14 @@ package com.MVCHibernate.repository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
 import com.MVCHibernate.model.Goal;
+import java.util.List;
 @Repository
 public class GoalRepositoryImpl implements GoalRepository {
 
@@ -22,5 +24,16 @@ public class GoalRepositoryImpl implements GoalRepository {
 		
 		return null;
 	}
+
+	public List getAllGoalls() {
+		
+		Query query = em.createQuery("Select g from Goal g");
+		
+		List goal = query.getResultList();
+		
+		return goal;
+	}
+
+
 
 }
