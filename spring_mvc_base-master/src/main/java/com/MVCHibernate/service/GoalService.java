35cd35b4c93da52c93f2f656e63f8df902cb3fb9
ammad <ammad@10.0.0.22>
2017-09-26 22:30:18 -0500

    Added findAllGoals method to GoalService interface to get the method to return all goals from database.

--- spring_mvc_base-master/src/main/java/com/MVCHibernate/service/GoalService.java
+++ spring_mvc_base-master/src/main/java/com/MVCHibernate/service/GoalService.java
@@ -1,9 +1,13 @@
 package com.MVCHibernate.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 import com.MVCHibernate.model.Goal;
 public interface GoalService {
 	Goal save(Goal goal);
+
+	List<Goal> findAllGoals();
 
 }
