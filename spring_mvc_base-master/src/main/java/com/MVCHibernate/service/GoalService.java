8ae7aa2261fed62be23d6c89ae86e0bcf036a640
ammad <ammad@10.0.0.22>
2017-09-28 21:45:21 -0500

    Added Method in Goal Servie interface to get GoalReport.

--- spring_mvc_base-master/src/main/java/com/MVCHibernate/service/GoalService.java
+++ spring_mvc_base-master/src/main/java/com/MVCHibernate/service/GoalService.java
@@ -5,9 +5,12 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 
 import com.MVCHibernate.model.Goal;
+import com.MVCHibernate.model.GoalReport;
 public interface GoalService {
 	Goal save(Goal goal);
 
 	List<Goal> findAllGoals();
+
+	 List<GoalReport> getAllGoalsReport();
 
 }
