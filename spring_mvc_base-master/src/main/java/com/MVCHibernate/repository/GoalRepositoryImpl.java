07b154308f03e336273cd04a0062e76258b17e2f
ammad <ammad@10.0.0.22>
2017-09-29 20:01:44 -0500

    Implemented named queries for two methods : getAllGoalls, getAllGoalsReport

--- spring_mvc_base-master/src/main/java/com/MVCHibernate/repository/GoalRepositoryImpl.java
+++ spring_mvc_base-master/src/main/java/com/MVCHibernate/repository/GoalRepositoryImpl.java
@@ -3,12 +3,15 @@ package com.MVCHibernate.repository;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
+import javax.persistence.TypedQuery;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
 import com.MVCHibernate.model.Goal;
+import com.MVCHibernate.model.GoalReport;
+
 import java.util.List;
 @Repository
 public class GoalRepositoryImpl implements GoalRepository {
@@ -28,7 +31,8 @@ public class GoalRepositoryImpl implements GoalRepository {
 	}
 	
 	public List getAllGoalsReport() {
-		Query query =em.createQuery("Select new com.MVCHibernate.model.GoalReport(g.minutes, e.minutes, e.activity)"+"from Goal g,Exercise e where g.id =e.goal.id ");
+	//	Query query =em.createQuery("Select new com.MVCHibernate.model.GoalReport(g.minutes, e.minutes, e.activity)"+"from Goal g,Exercise e where g.id =e.goal.id ");
+		TypedQuery<GoalReport> query=em.createNamedQuery(Goal.Get_All_GoalsReport,GoalReport.class);
 		
 		List goalReports = query.getResultList();
 		return goalReports;
@@ -36,7 +40,8 @@ public class GoalRepositoryImpl implements GoalRepository {
 
 	public List getAllGoalls() {
 		
-		Query query = em.createQuery("Select g from Goal g");
+	//	Query query = em.createQuery("Select g from Goal g");
+		TypedQuery<Goal> query=em.createNamedQuery(Goal.Get_All_Goals,Goal.class);
 		
 		List goal = query.getResultList();
 		
