eb5430046d93c596f3c57cde2752e2f7d01be2d5
ammad <ammad@10.0.0.22>
2017-09-30 16:57:11 -0500

    Extended GoalRepository Interface with Spring Data JPa, also provided a custom query and removed named queries from the goal.

--- spring_mvc_base-master/src/main/java/com/MVCHibernate/repository/GoalRepository.java
+++ spring_mvc_base-master/src/main/java/com/MVCHibernate/repository/GoalRepository.java
@@ -2,12 +2,18 @@ package com.MVCHibernate.repository;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.MVCHibernate.model.Goal;
+import com.MVCHibernate.model.GoalReport;
+@Repository
+public interface GoalRepository extends JpaRepository<Goal,Long> {
+	
+	@Query("Select new com.MVCHibernate.model.GoalReport(g.minutes, e.minutes, e.activity)"+"from Goal g,Exercise e where g.id =e.goal.id")
+	 List<GoalReport> getAllGoalsReport();
+	
+	
 
-public interface GoalRepository {
-	Goal save(Goal goal);
-	 List getAllGoalls(); 
-	 List getAllGoalsReport();
 }
