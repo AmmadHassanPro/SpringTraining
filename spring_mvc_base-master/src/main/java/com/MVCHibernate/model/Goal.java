9dcebc47208aa283a3748e1c9c87d9e875e8f69f
ammad <ammad@10.0.0.22>
2017-09-29 20:00:24 -0500

    Added Named Queries on the Goal Object, to increase the readability of code.

--- spring_mvc_base-master/src/main/java/com/MVCHibernate/model/Goal.java
+++ spring_mvc_base-master/src/main/java/com/MVCHibernate/model/Goal.java
@@ -7,6 +7,8 @@ import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
@@ -15,7 +17,20 @@ import org.hibernate.validator.constraints.Range;
 
 @Entity
 @Table(name="goals")
+@NamedQueries({
+@NamedQuery(name=Goal.Get_All_Goals, query = "Select g from Goal g")	,
+
+@NamedQuery(name=Goal.Get_All_GoalsReport, query = "Select new com.MVCHibernate.model.GoalReport(g.minutes, e.minutes, e.activity)"+"from Goal g,Exercise e where g.id =e.goal.id")
+	
+	
+})
+
 public class Goal {
+	
+	public static final String Get_All_Goals = "getAllGoalls";
+	public static final String Get_All_GoalsReport = "getAllGoalsReport";
+	
+	
 	@Id
 	@GeneratedValue
 	private Long id;
