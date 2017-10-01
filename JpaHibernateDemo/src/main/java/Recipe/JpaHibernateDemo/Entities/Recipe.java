2b41b9673983f8e24890ebb5dbf07d79f072ebc1
ammad <ammad@10.0.0.22>
2017-10-01 17:44:56 -0500

    Modified Recipe to include Difficulty Enum.

--- JpaHibernateDemo/src/main/java/Recipe/JpaHibernateDemo/Entities/Recipe.java
+++ JpaHibernateDemo/src/main/java/Recipe/JpaHibernateDemo/Entities/Recipe.java
@@ -5,6 +5,8 @@ import java.util.Set;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -24,7 +26,22 @@ public class Recipe {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	public Long id;
 	
-	 	private String description;
+		 @Enumerated(value = EnumType.STRING)
+		 private Difficulty difficulty;
+	
+	 	public Difficulty getDifficulty() {
+			return difficulty;
+		}
+		public void setDifficulty(Difficulty difficulty) {
+			this.difficulty = difficulty;
+		}
+		public Set<Category> getCategories() {
+			return categories;
+		}
+		public void setCategories(Set<Category> categories) {
+			this.categories = categories;
+		}
+		private String description;
 	 
 	    private Integer prepTime;
 
