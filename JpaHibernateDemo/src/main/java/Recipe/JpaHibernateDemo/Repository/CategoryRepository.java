e6e786622d5a09522ad3661cab30e5079d011dd0
ammad <ammad@10.0.0.22>
2017-10-01 19:01:41 -0500

    Added findByDescription to be able to fetch data by Description

--- JpaHibernateDemo/src/main/java/Recipe/JpaHibernateDemo/Repository/CategoryRepository.java
+++ JpaHibernateDemo/src/main/java/Recipe/JpaHibernateDemo/Repository/CategoryRepository.java
@@ -1,9 +1,13 @@
 package Recipe.JpaHibernateDemo.Repository;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 
 import Recipe.JpaHibernateDemo.Entities.Category;
 
 public interface CategoryRepository extends CrudRepository<Category,Long>{
 
+	Optional<Category> findByDescription(String description);
+	
 }
