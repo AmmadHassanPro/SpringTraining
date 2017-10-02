cbfaf1b90e2955d31548d85b70cfa56b51d60747
ammad <ammad@10.0.0.22>
2017-10-01 19:02:22 -0500

    Added findByDescription to be able to fetch data by Description.

--- JpaHibernateDemo/src/main/java/Recipe/JpaHibernateDemo/Repository/UnitOfMeasureRepository.java
+++ JpaHibernateDemo/src/main/java/Recipe/JpaHibernateDemo/Repository/UnitOfMeasureRepository.java
@@ -1,9 +1,13 @@
 package Recipe.JpaHibernateDemo.Repository;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 
 import Recipe.JpaHibernateDemo.Entities.UnitOfMeasure;
 
 public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure,Long>{
 
+// We can create the methods like 'find' By 'attribute, or field name' and in the parameter, the argument must match the field name in the table exactly
+Optional<UnitOfMeasure> findByDescription(String description);
 }
