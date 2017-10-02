25eb042ed5807c3e3a4522c229543a869d77905a
ammad <ammad@10.0.0.22>
2017-10-01 19:02:55 -0500

    Changed attribute spelling from "Description" to "description".

--- JpaHibernateDemo/src/main/java/Recipe/JpaHibernateDemo/Entities/UnitOfMeasure.java
+++ JpaHibernateDemo/src/main/java/Recipe/JpaHibernateDemo/Entities/UnitOfMeasure.java
@@ -12,6 +12,7 @@ public class UnitOfMeasure {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	private String description;
 	public Long getId() {
 		return id;
 	}
@@ -19,11 +20,11 @@ public class UnitOfMeasure {
 		this.id = id;
 	}
 	public String getDescription() {
-		return Description;
+		return description;
 	}
 	public void setDescription(String description) {
-		Description = description;
+		description = description;
 	}
-	private String Description;	
+		
 
 }
