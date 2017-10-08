d68036a5cd1ec665340ebfb41fe34ee969b2cf77
ammad <ammad@10.0.0.22>
2017-10-07 20:20:00 -0500

    Added User Agent as Chrome, as we are fetching image for recipes from websites.

--- JpaHibernateDemo/src/main/java/Recipe/JpaHibernateDemo/JpaHibernateDemoApplication.java
+++ JpaHibernateDemo/src/main/java/Recipe/JpaHibernateDemo/JpaHibernateDemoApplication.java
@@ -2,11 +2,15 @@ package Recipe.JpaHibernateDemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("Recipe.JpaHibernateDemo")
 public class JpaHibernateDemoApplication {
 
 	public static void main(String[] args) {
+		System.setProperty("http.agent", "Chrome");
 		SpringApplication.run(JpaHibernateDemoApplication.class, args);
+		
 	}
 }
