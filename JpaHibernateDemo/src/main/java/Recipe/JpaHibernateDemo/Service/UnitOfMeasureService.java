cf3f64f107ba9219e366130eb68f02d3d3e39d68
ammad <ammad@10.0.0.22>
2017-10-07 20:18:41 -0500

    A Service Layer for Unit of Measure

---
+++ JpaHibernateDemo/src/main/java/Recipe/JpaHibernateDemo/Service/UnitOfMeasureService.java
@@ -0,0 +1,35 @@
+package Recipe.JpaHibernateDemo.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import Recipe.JpaHibernateDemo.Entities.UnitOfMeasure;
+import Recipe.JpaHibernateDemo.Repository.UnitOfMeasureRepository;
+
+@Service
+public class UnitOfMeasureService {
+	
+@Autowired
+private UnitOfMeasureRepository uom_repo;
+@Autowired
+private List<UnitOfMeasure> uom_list;
+
+public UnitOfMeasure findByDescription(String description){
+
+	Optional<UnitOfMeasure> uom_optional = uom_repo.findByDescription(description);
+	
+	UnitOfMeasure uom = uom_optional.get();
+	
+	return uom;
+	
+	
+}
+
+
+
+	
+
+}
