c89e76cef882aaaebee88e983ff937facdeaf9be
ammad <ammad@10.0.0.22>
2017-10-07 20:20:50 -0500

    Marking Notes as Component

--- JpaHibernateDemo/src/main/java/Recipe/JpaHibernateDemo/Entities/Notes.java
+++ JpaHibernateDemo/src/main/java/Recipe/JpaHibernateDemo/Entities/Notes.java
@@ -8,9 +8,29 @@ import javax.persistence.Lob;
 import javax.persistence.OneToOne;
 
 import org.hibernate.annotations.Generated;
+import org.springframework.stereotype.Component;
 @Entity
+@Component
 public class Notes {
 	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public Recipe getRecipe() {
+		return recipe;
+	}
+	public void setRecipe(Recipe recipe) {
+		this.recipe = recipe;
+	}
+	public String getRecipeNotes() {
+		return recipeNotes;
+	}
+	public void setRecipeNotes(String recipeNotes) {
+		this.recipeNotes = recipeNotes;
+	}
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
