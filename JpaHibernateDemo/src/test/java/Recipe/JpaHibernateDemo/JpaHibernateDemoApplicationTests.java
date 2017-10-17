be569ba4b595e3b8b4fcef10c342f7bf43f382af
ammad <ammad@10.0.0.22>
2017-10-16 22:45:45 -0500

    Cleaned some unused code for JpaHibernateDemoApplicationTests.

--- JpaHibernateDemo/src/test/java/Recipe/JpaHibernateDemo/JpaHibernateDemoApplicationTests.java
+++ JpaHibernateDemo/src/test/java/Recipe/JpaHibernateDemo/JpaHibernateDemoApplicationTests.java
@@ -1,38 +1,16 @@
 package Recipe.JpaHibernateDemo;
 
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.ui.Model;
-
-import Recipe.JpaHibernateDemo.Controller.RecipeController;
-import Recipe.JpaHibernateDemo.Service.RecipeService;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class JpaHibernateDemoApplicationTests {
-	
-	private RecipeService recpie_service;
-	
-	private RecipeController recipeController;
-	private Model model;
-	
-	
-	@Before
-	public void setUp() throws Exception{
-		MockitoAnnotations.initMocks(recpie_service);
-		MockitoAnnotations.initMocks(model);
-		recpie_service = new RecipeService();
-		
-	}
-	
+
 	@Test
-	public void getRecipeList() throws Exception{
-		
+	public void contextLoads() {
 		
 	}
 
